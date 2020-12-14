b6d2f8d9c9422676ae6bc100ea713a747c312989
NECOMAbot <n88.inokuchi.yuma@gmail.com>
2020-12-14 12:39:36 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname,                      presence: true
-  validates :last_name_chinese_characters,  presence: true
-  validates :first_name_chinese_characters, presence: true
-  validates :last_name_katakana,            presence: true
-  validates :first_name_katakana,           presence: true
-  validates :birthday,                      presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+    with_options presence: true, format: {with: /\A[一-龥]+\z/ } do
+      validates :last_name_chinese_characters
+      validates :first_name_chinese_characters
+    end
+    with_options presence: true, format: {with: /\A[ァ-ヶー－]+\z/ } do
+      validates :last_name_katakana
+      validates :first_name_katakana
+    end
+    validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i}
+  end
 
   has_many :items
   has_many :itme_purchases

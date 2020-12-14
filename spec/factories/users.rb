f754355e16d719ae4b3b1d3e1868c4883548236d
NECOMAbot <n88.inokuchi.yuma@gmail.com>
2020-12-14 12:39:31 +0900

    Update users.rb

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,14 +1,14 @@
 FactoryBot.define do
   factory :user do
     nickname {Faker::Name.name}
-    last_name_chinese_characters {Faker::Name.last_name}
-    first_name_chinese_characters {Faker::Name.first_name}
-    last_name_katakana {Faker::Name.last_name}
-    first_name_katakana {Faker::Name.first_name}
+    last_name_chinese_characters {"山田"}
+    first_name_chinese_characters {"太郎"}
+    last_name_katakana {"ヤマダ"}
+    first_name_katakana {"タロウ"}
     birthday {Faker::Date.birthday}
     email {Faker::Internet.free_email}
     password = Faker::Internet.password(min_length: 6)
-    password {password}
-    password_confirmation {password}
+    password {"hoge12"}
+    password_confirmation {"hoge12"}
   end
 end

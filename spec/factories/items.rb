9728c1f5079a1cc8342d54082bf6d68eebbc327b
NECOMAbot <n88.inokuchi.yuma@gmail.com>
2020-12-18 18:34:43 +0900

    Update items.rb

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,12 +1,16 @@
 FactoryBot.define do
   factory :item do
-    name {Faker::Name.name}
-    explanation {Faker::String.random}
-    category_id {1}
-    status_id {1}
-    shipping_fee_burden_id {1}
-    prefecture_id {1}
-    days_to_ship_id {1}
-    price {Faker::Alphanumeric.random}
+    name {"商品の名前"}
+    explanation {"商品の説明"}
+    category_id {2}
+    status_id {2}
+    shipping_fee_burden_id {2}
+    prefecture_id {2}
+    days_to_ship_id {2}
+    price {300}
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/apple-touch-icon.png'), filename: 'apple-touch-icon.png')
+    end
+    association :user
   end
 end

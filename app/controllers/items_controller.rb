57a1d47a5176d44d5dea1321f5de6e1f60bbd6f9
NECOMAbot <n88.inokuchi.yuma@gmail.com>
2020-12-19 23:58:02 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,10 +18,18 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:name, :explanation, :category_id, :status_id, :shipping_fee_burden_id, :prefecture_id, :days_to_ship_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
 end

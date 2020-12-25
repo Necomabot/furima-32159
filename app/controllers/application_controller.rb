c74b57583ede609896b527ec040b33f1f9ee6eed
NECOMAbot <n88.inokuchi.yuma@gmail.com>
2020-12-25 16:34:40 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,11 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :basic_auth
 
-  
+
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name_chinese_characters, :first_name_chinese_characters, :last_name_katakana, :first_name_katakana, :birthday])
-  
+  end
 
 
   def basic_auth

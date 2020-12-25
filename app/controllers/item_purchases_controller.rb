ce73f68038db02a58888b9d70b88e052e009241c
NECOMAbot <n88.inokuchi.yuma@gmail.com>
2020-12-25 14:01:03 +0900

    Update item_purchases_controller.rb

--- app/controllers/item_purchases_controller.rb
+++ app/controllers/item_purchases_controller.rb
@@ -1,2 +1,43 @@
 class ItemPurchasesController < ApplicationController
+  before_action :set_item
+  before_action :authenticate_user!
+
+  def index
+    @item_purchase_shipping_address = ItemPurchaseShippingAddress.new
+
+  end
+
+  def new
+  end
+
+  def create
+    @item_purchase_shipping_address = ItemPurchaseShippingAddress.new(item_purchase_shipping_address_params)
+    if @item_purchase_shipping_address.valid?
+      pay_item
+      @item_purchase_shipping_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def item_purchase_shipping_address_params
+    params.require(:item_purchase_shipping_address).permit(:postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: item_purchase_shipping_address_params[:token],
+      currency: 'jpy'
+    )
+  end
+
 end

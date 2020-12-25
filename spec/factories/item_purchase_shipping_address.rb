9e34819b7d791b3519107ae285307bba84dc11d3
NECOMAbot <n88.inokuchi.yuma@gmail.com>
2020-12-25 14:01:25 +0900

    Create item_purchase_shipping_address.rb

---
+++ spec/factories/item_purchase_shipping_address.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :item_purchase_shipping_address do
+    token {"tok_abcdefghijk00000000000000000"}
+    postal_code {"123-4567"}
+    prefecture_id {2}
+    municipality {"横浜市緑区"}
+    address {"青山1-1-1"}
+    phone_number {"01234567891"}
+  end
+end
